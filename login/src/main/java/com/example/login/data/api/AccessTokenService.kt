7e09e9c6b24ea4fa23c37ad041b5377ca9ce824e
a.beigi <Ch@1234567>
2024-10-20 19:13:42 +0330

    Refactor: Moves AccessTokenService to login module

--- login/src/main/java/com/example/login/data/api/AccessTokenService.kt
+++ login/src/main/java/com/example/login/data/api/AccessTokenService.kt
@@ -1,5 +1,7 @@
-package com.jabama.challenge.network.oauth
+package com.example.login.data.api
 
+import com.example.login.data.model.RequestAccessToken
+import com.example.login.data.model.ResponseAccessToken
 import kotlinx.coroutines.Deferred
 import retrofit2.http.Body
 import retrofit2.http.Headers
@@ -8,5 +10,5 @@ import retrofit2.http.POST
 interface AccessTokenService {
     @Headers("Accept:application/json")
     @POST("https://github.com/login/oauth/access_token")
-    fun accessToken(@Body requestAccessToken: RequestAccessToken) : Deferred<ResponseAccessToken>
+    fun accessToken(@Body requestAccessToken: RequestAccessToken): Deferred<ResponseAccessToken>
 }
