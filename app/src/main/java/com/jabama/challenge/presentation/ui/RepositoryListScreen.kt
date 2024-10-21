743ed4b8a1a9f79e16bc92a355fe56d73af1845f
a.beigi <Ch@1234567>
2024-10-21 19:51:33 +0330

    Refactors AuthorizeViewModel.kt and adds search to  RepositoriesScreen

--- app/src/main/java/com/jabama/challenge/presentation/ui/RepositoryListScreen.kt
+++ app/src/main/java/com/jabama/challenge/presentation/ui/RepositoryListScreen.kt
@@ -3,66 +3,91 @@ package com.jabama.challenge.presentation.ui
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.CircularProgressIndicator
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.lifecycle.LiveData
 import com.jabama.challenge.base.Failed
 import com.jabama.challenge.base.LoadableData
 import com.jabama.challenge.base.Loaded
 import com.jabama.challenge.base.Loading
+import com.jabama.challenge.base.NotLoaded
 import com.jabama.challenge.domain.model.Repository
 import com.jabama.challenge.github.R
 
 @Composable
 fun RepositoriesScreen(
-    loadableRepositories: LiveData<LoadableData<List<Repository>>>,
-    onStarClicked: (Repository) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    loadableRepositories: LoadableData<List<Repository>>,
+    onSearchValueChange: (String) -> Unit,
+    keyword: String,
 ) {
-    when (val it = loadableRepositories.observeAsState().value) {
+    Column(modifier = modifier.fillMaxSize()) {
+        SearchBar(
+            modifier = Modifier,
+            onValueChange = onSearchValueChange,
+            keyword = keyword,
+        )
+        Spacer(Modifier.height(32.dp))
+        LoadableRepositoryListComponent(loadableRepositories = loadableRepositories)
+    }
+
+}
+
+@Composable
+private fun LoadableRepositoryListComponent(
+    loadableRepositories: LoadableData<List<Repository>>,
+    modifier: Modifier = Modifier,
+) {
+    when (loadableRepositories) {
         is Failed -> {
-            Toast.makeText(LocalContext.current, it.throwble.message, Toast.LENGTH_SHORT).show()
+            Toast.makeText(
+                LocalContext.current,
+                loadableRepositories.throwble.message,
+                Toast.LENGTH_SHORT
+            ).show()
         }
-        is Loading -> {
+
+        is Loading, NotLoaded -> {
             Box(modifier = modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
                 CircularProgressIndicator(color = Color.Gray)
             }
         }
+
         is Loaded -> {
-            RepositoryList(
-                list = loadableRepositories.observeAsState().value?.data ?: listOf(),
-                onStarClicked = onStarClicked,
+            LoadedRepositoryList(
+                list = loadableRepositories.data,
                 modifier = modifier
             )
         }
-        else -> {}
     }
 }
 
 @Composable
-fun RepositoryList(
+fun LoadedRepositoryList(
     list: List<Repository>,
-    onStarClicked: (Repository) -> Unit,
     modifier: Modifier = Modifier,
 ) {
     val repositoryListState = rememberLazyListState()
@@ -78,7 +103,6 @@ fun RepositoryList(
         items(list) { item ->
             RepositoryItem(
                 repository = item,
-                onStarClicked = { onStarClicked(it) },
                 modifier = Modifier
             )
         }
@@ -88,13 +112,8 @@ fun RepositoryList(
 @Composable
 fun RepositoryItem(
     repository: Repository,
-    onStarClicked: (Repository) -> Unit,
     modifier: Modifier = Modifier
 ) {
-    var isFavorite by remember {
-        mutableStateOf(repository.isFavorite)
-    }
-
     Column(
         modifier = modifier
             .fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
@@ -122,18 +141,6 @@ fun RepositoryItem(
                     .padding(vertical = 4.dp, horizontal = 8.dp)
             )
             Spacer(modifier = Modifier.weight(1f))
-            IconButton(onClick = {
-                isFavorite = !isFavorite
-                onStarClicked(repository)
-            }) {
-                Icon(
-                    painter = painterResource(
-                        id = if (isFavorite) R.drawable.ic_star_fav_24
-                        else R.drawable.ic_star
-                    ),
-                    contentDescription = null
-                )
-            }
         }
         Spacer(modifier = Modifier.size(8.dp))
         Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
@@ -144,13 +151,6 @@ fun RepositoryItem(
                     fontWeight = FontWeight.Normal
                 )
             )
-            Spacer(modifier = Modifier.size(8.dp))
-            if (isFavorite) {
-                Icon(
-                    painter = painterResource(id = R.drawable.ic_star_fav_24),
-                    contentDescription = null
-                )
-            }
             Spacer(modifier = Modifier.size(8.dp))
             Text(
                 text = repository.updated_at, style = TextStyle(
--- app/src/main/java/com/jabama/challenge/presentation/viewmodel/AuthorizeViewModel.kt
+++ app/src/main/java/com/jabama/challenge/presentation/viewmodel/AuthorizeViewModel.kt
@@ -1,4 +1,4 @@
-package com.jabama.challenge.presentation
+package com.jabama.challenge.presentation.viewmodel
 
 import android.net.Uri
 import androidx.lifecycle.ViewModel
@@ -9,28 +9,25 @@ import com.jabama.challenge.base.LoadableData
 import com.jabama.challenge.base.Loaded
 import com.jabama.challenge.base.Loading
 import com.jabama.challenge.base.NotLoaded
-import com.jabama.challenge.domain.model.Repository
 import com.jabama.challenge.domain.model.RequestAccessToken
 import com.jabama.challenge.domain.model.ResponseAccessToken
 import com.jabama.challenge.domain.usecase.GetAccessTokenUseCase
-import com.jabama.challenge.domain.usecase.GetRepositoryListUseCase
-import com.jabama.challenge.domain.usecase.ReadTokenUseCase
 import com.jabama.challenge.domain.usecase.SaveTokenUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-const val CLIENT_ID = "Ov23lieNfZen0Ac2lX66"
-const val CLIENT_SECRET = "b49541b80e0b3db47a8e5a785749f1fdf32baa27"
-const val REDIRECT_URI = "https://github.com/AysudaBeigi/JabamaCodeChallenge"
-const val url =
+private const val CLIENT_ID = "Ov23lieNfZen0Ac2lX66"
+private const val CLIENT_SECRET = "b49541b80e0b3db47a8e5a785749f1fdf32baa27"
+private const val REDIRECT_URI = "https://github.com/AysudaBeigi/JabamaCodeChallenge"
+private const val url =
     "https://github.com/login/oauth/authorize?client_id=$CLIENT_ID&redirect_uri=$REDIRECT_URI&scope=repo user&state=0"
+
+private const val CODE = "code"
 
 class GithubViewModel(
     private val saveTokenUseCase: SaveTokenUseCase,
-    private val readTokenUseCase: ReadTokenUseCase,
     private val getAccessTokenUseCase: GetAccessTokenUseCase,
-    private val getRepositoryListUseCase: GetRepositoryListUseCase,
     private val coroutineDispatcherProvider: CoroutineDispatcherProvider,
 ) : ViewModel() {
 
@@ -38,11 +35,10 @@ class GithubViewModel(
 
     data class GithubState(
         val loadableToken: LoadableData<ResponseAccessToken> = NotLoaded,
-        val loadableRepositories: LoadableData<List<Repository>> = NotLoaded,
     )
 
     init {
-        Uri.parse(url).getQueryParameter("code")?.let {
+        Uri.parse(url).getQueryParameter(CODE)?.let {
             authorize(it)
         }
     }
@@ -71,26 +67,6 @@ class GithubViewModel(
             }.onFailure { throwable ->
                 _state.update {
                     it.copy(loadableToken = Failed(throwable))
-                }
-            }
-
-        }
-    }
-
-    fun getRepositories(token: String) {
-        viewModelScope.launch(coroutineDispatcherProvider.ioDispatcher()) {
-            _state.update {
-                it.copy(loadableRepositories = Loading)
-            }
-            runCatching {
-                getRepositoryListUseCase.execute(token = token)
-            }.onSuccess { repositoryList ->
-                _state.update {
-                    it.copy(loadableRepositories = Loaded(repositoryList))
-                }
-            }.onFailure { throwable ->
-                _state.update {
-                    it.copy(loadableRepositories = Failed(throwable))
                 }
             }
 
