f8570ab5a994a357807d268edb6d16c1f4afdba6
a.beigi <Ch@1234567>
2024-10-21 12:04:02 +0330

    Fixes AccessTokenService : change output to domain model

--- app/src/main/java/com/jabama/challenge/data/repository/AccessTokenDataSourceImpl.kt
+++ app/src/main/java/com/jabama/challenge/data/repository/AccessTokenDataSourceImpl.kt
@@ -1,11 +1,12 @@
 package com.jabama.challenge.data.repository
 
 import com.jabama.challenge.data.api.AccessTokenService
+import com.jabama.challenge.data.mapper.mapToResponseAccessToken
 import com.jabama.challenge.data.model.RequestAccessTokenDto
 import com.jabama.challenge.domain.IAccessTokenDataSource
 
-const val TOKEN = "TOKEN"
 class IAccessTokenDataSourceImpl(private val accessTokenService: AccessTokenService) :
     IAccessTokenDataSource {
-    override fun accessToken(requestAccessTokenDto: RequestAccessTokenDto) = accessTokenService.accessToken(requestAccessTokenDto)
+    override suspend fun accessToken(requestAccessTokenDto: RequestAccessTokenDto) =
+        accessTokenService.accessToken(requestAccessTokenDto).mapToResponseAccessToken()
 }
