99ef667575a387903237493d7187223199ab74e4
a.beigi <Ch@1234567>
2024-10-21 20:14:02 +0330

    Refactors AuthorizeViewModel.kt

--- app/src/main/java/com/jabama/challenge/presentation/viewmodel/AuthorizeViewModel.kt
+++ app/src/main/java/com/jabama/challenge/presentation/viewmodel/AuthorizeViewModel.kt
@@ -1,6 +1,5 @@
 package com.jabama.challenge.presentation.viewmodel
 
-import android.net.Uri
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.jabama.challenge.base.CoroutineDispatcherProvider
@@ -9,21 +8,15 @@ import com.jabama.challenge.base.LoadableData
 import com.jabama.challenge.base.Loaded
 import com.jabama.challenge.base.Loading
 import com.jabama.challenge.base.NotLoaded
-import com.jabama.challenge.domain.model.RequestAccessToken
 import com.jabama.challenge.domain.model.ResponseAccessToken
 import com.jabama.challenge.domain.usecase.GetAccessTokenUseCase
 import com.jabama.challenge.domain.usecase.SaveTokenUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-private const val CLIENT_ID = "Ov23lieNfZen0Ac2lX66"
-private const val CLIENT_SECRET = "b49541b80e0b3db47a8e5a785749f1fdf32baa27"
-private const val REDIRECT_URI = "https://github.com/AysudaBeigi/JabamaCodeChallenge"
-private const val url =
-    "https://github.com/login/oauth/authorize?client_id=$CLIENT_ID&redirect_uri=$REDIRECT_URI&scope=repo user&state=0"
-
-private const val CODE = "code"
 
 class GithubViewModel(
     private val saveTokenUseCase: SaveTokenUseCase,
@@ -32,38 +25,26 @@ class GithubViewModel(
 ) : ViewModel() {
 
     private val _state: MutableStateFlow<GithubState> = MutableStateFlow(GithubState())
+    val state: StateFlow<GithubState> = _state.asStateFlow()
 
     data class GithubState(
         val loadableToken: LoadableData<ResponseAccessToken> = NotLoaded,
     )
 
-    init {
-        Uri.parse(url).getQueryParameter(CODE)?.let {
-            authorize(it)
-        }
-    }
-
-    fun authorize(code: String) {
+    fun authorize() {
         viewModelScope.launch(coroutineDispatcherProvider.ioDispatcher()) {
             _state.update {
                 it.copy(loadableToken = Loading)
             }
             runCatching {
-                getAccessTokenUseCase.execute(
-                    requestAccessToken = RequestAccessToken(
-                        code = code,
-                        state = "0",
-                        clientSecret = CLIENT_SECRET,
-                        clientId = CLIENT_ID,
-                        redirectUri = REDIRECT_URI,
-                    )
-                )
-
+                getAccessTokenUseCase.execute()
             }.onSuccess { token ->
-                _state.update {
-                    it.copy(loadableToken = Loaded(token))
+                token?.let {
+                    _state.update {
+                        it.copy(loadableToken = Loaded(token))
+                    }
+                    saveTokenUseCase.execute(token = token.accessToken)
                 }
-                saveTokenUseCase.execute(token = token.accessToken)
             }.onFailure { throwable ->
                 _state.update {
                     it.copy(loadableToken = Failed(throwable))
