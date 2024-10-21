705d79f19eb9a3b112c113647593f2ba4c9ce2c8
a.beigi <Ch@1234567>
2024-10-21 11:53:33 +0330

    Adds Mapper.kt

---
+++ app/src/main/java/com/jabama/challenge/data/mapper/Mapper.kt
@@ -0,0 +1,19 @@
+package com.jabama.challenge.data.mapper
+
+import com.jabama.challenge.data.model.RequestAccessTokenDto
+import com.jabama.challenge.data.model.ResponseAccessTokenDto
+import com.jabama.challenge.domain.model.RequestAccessToken
+import com.jabama.challenge.domain.model.ResponseAccessToken
+
+internal fun RequestAccessTokenDto.mapToRequestAccessToken() = RequestAccessToken(
+    clientId = clientId,
+    clientSecret = clientSecret,
+    code = code,
+    redirectUri = redirectUri,
+    state = state,
+)
+
+internal fun ResponseAccessTokenDto.mapToResponseAccessToken() = ResponseAccessToken(
+    accessToken = accessToken,
+    tokenType = tokenType,
+)
