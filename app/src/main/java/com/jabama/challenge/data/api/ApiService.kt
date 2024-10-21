662d86596c431de01a78141452e8c674cad5a98e
a.beigi <Ch@1234567>
2024-10-21 19:13:12 +0330

    Adds api for get repository

--- app/src/main/java/com/jabama/challenge/data/api/ApiService.kt
+++ app/src/main/java/com/jabama/challenge/data/api/ApiService.kt
@@ -1,13 +1,21 @@
 package com.jabama.challenge.data.api
 
+import com.jabama.challenge.data.model.RepositoryDto
 import com.jabama.challenge.data.model.RequestAccessTokenDto
 import com.jabama.challenge.data.model.ResponseAccessTokenDto
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.Headers
 import retrofit2.http.POST
 
-interface AccessTokenService {
+interface ApiService {
     @Headers("Accept:application/json")
     @POST("https://github.com/login/oauth/access_token")
     fun accessToken(@Body requestAccessTokenDto: RequestAccessTokenDto): ResponseAccessTokenDto
+
+    @GET("user/repos")
+    fun getUserRepositories(
+        @Header("Authorization") token: String
+    ):List<RepositoryDto>
 }
